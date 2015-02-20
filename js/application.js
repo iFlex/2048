071f90460a0d7560d9b74364567b57fdc0dc638b
iFlex <milorad.liviu.felix@gmail.com>
2015-02-20 06:09:31 +0000

    Improved the driver to work for both injection (overrides the existing GameManager) or normal setup ( GameManager is provided by application.js)

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var driver = new Driver2048();
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  driver.setGameManager(new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager));
 });
